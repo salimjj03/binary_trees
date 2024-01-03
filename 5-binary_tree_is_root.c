b249c239ea05e35c5718ff8b5aed987de3ca1754
salim aliyu musawa <salemaliyu1992@gmail.co>
2024-01-03 14:13:52 -0800

    is_root

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - ...
+ * @node: ...
+ *
+ * Return: ...
+ */
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL || node->parent != NULL)
+		return (0);
+	return (1);
+}
