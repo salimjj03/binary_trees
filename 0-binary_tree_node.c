c9b58c932b137a6a3b276893491805c449d74bc4
salim aliyu musawa <salemaliyu1992@gmail.co>
2024-01-03 11:28:17 -0800

    create tree node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - ...
+ * @parent: ...
+ * @value: ...
+ *
+ * Return: ...
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newnode;
+
+	newnode = malloc(sizeof(binary_tree_t));
+	if (newnode == NULL)
+		return (NULL);
+
+	newnode->n = value;
+	newnode->parent = parent;
+	newnode->left = NULL;
+	newnode->right = NULL;
+
+	return (newnode);
+}
