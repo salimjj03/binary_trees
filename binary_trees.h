b3db88dc18b9dc6a41249a8ae415adda6fcb357d
salim aliyu musawa <salemaliyu1992@gmail.co>
2024-01-04 14:47:26 -0800

    ancestors

--- binary_trees.h
+++ binary_trees.h
@@ -45,5 +45,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 void binary_tree_print(const binary_tree_t *);
 int power(int base, int exponent);
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second);
 
 #endif
